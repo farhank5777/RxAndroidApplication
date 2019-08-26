8456b3a03e6ec4e76308be6f8ea158245a07b223
farhank5777 <farhank5777@gmail.com>
2019-08-26 16:39:38 +0530

    operator example1

--- app/src/main/java/com/example/rxandroidapplication/OperatorExample.java
+++ app/src/main/java/com/example/rxandroidapplication/OperatorExample.java
@@ -19,7 +19,7 @@ public class OperatorExample extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // fromArray
         Integer[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 21};
 
         Observable.fromArray(number)
                 .subscribeOn(Schedulers.io())
