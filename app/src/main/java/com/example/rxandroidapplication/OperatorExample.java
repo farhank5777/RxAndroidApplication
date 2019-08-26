55188eed4b50bb235e2045747612abba303eb052
farhank5777 <farhank5777@gmail.com>
2019-08-26 16:24:35 +0530

    operator example

---
+++ app/src/main/java/com/example/rxandroidapplication/OperatorExample.java
@@ -0,0 +1,13 @@
+package com.example.rxandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperatorExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
